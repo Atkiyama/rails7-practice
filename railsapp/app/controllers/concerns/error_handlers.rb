cb98f987524bb714105c4b9a7a79757ba658e1f2
Atkiyama <thisisatkiyama@gmail.com>
2025-01-03 16:03:42 +0900

    8章終了

--- railsapp/app/controllers/concerns/error_handlers.rb
+++ railsapp/app/controllers/concerns/error_handlers.rb
@@ -10,19 +10,23 @@ module ErrorHandlers
   end
 
   private def rescue400(e)
+    Rails.logger.error "Bad request: #{e.message}"
     render "errors/bad_request", status: 400
   end
 
   private def rescue403(e)
     @exception = e
+    Rails.logger.error "Forbidden: #{e.message}"
     render "errors/forbidden", status: 403
   end
 
   private def rescue404(e)
+    Rails.logger.error "Record not found: #{e.message}"
     render "errors/not_found", status: 404
   end
 
   private def rescue500(e)
+    Rails.logger.error "Internal Server Error: #{e.message}"
     render "errors/internal_server_error", status: 500
   end
 end
--- railsapp/app/controllers/staff/sessions_controller.rb
+++ railsapp/app/controllers/staff/sessions_controller.rb
@@ -8,19 +8,33 @@ class Staff::SessionsController < Staff::Base
     end
   end
 
-  def create
-    Rails.logger.info "Logged in"
-    @form = Staff::LoginForm.new(params[:staff_login_form])
-    if @form.email.present?
-      staff_members = StaffMember.find_by("email = ?", @form.email.downcase)
-    end
-    if Staff::Authenticator.new(staff_members).authenticate(@form.password)
-      session[:staff_member_id] = staff_members.id
-      redirect_to :staff_root
-    else
-      render action: 'new'
-    end
+def create
+  Rails.logger.info "Logged in"
+  @form = Staff::LoginForm.new(params[:staff_login_form])
+  Rails.logger.info "フォームの初期化完了"
+  
+  if @form.email.present?
+    staff_member = StaffMember.find_by("email = ?", @form.email.downcase)
   end
+  Rails.logger.info "スタッフメンバーの検索完了"
+  
+  if staff_member && Staff::Authenticator.new(staff_member).authenticate(@form.password)
+    if staff_member.suspended?
+      Rails.logger.info "アカウント停止"
+      flash.now.alert = "アカウントが停止されています。"
+      render action: "new"
+    else
+      Rails.logger.info "ログインしました"
+      session[:staff_member_id] = staff_member.id
+      flash.notice = "ログインしました。"
+      redirect_to :staff_root
+    end
+  else
+    Rails.logger.info "存在しない"
+    flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
+    render action: 'new'
+  end
+end
 
   def destroy
     # if current_staff_member
