6efe83a907829e798701f46ba63b80530f592c72
Atkiyama <thisisatkiyama@gmail.com>
2025-01-03 16:43:54 +0900

    9章終了

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resource :account
   end
   namespace :admin do
     root 'top#index'
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+    resources :staff_members
   end
   namespace :customer do
     root 'top#index'
